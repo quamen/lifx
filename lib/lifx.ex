144fef0581613c230c7d49d52850ef191a22660e
Gareth Townsend <gareth.townsend@me.com>
2015-08-20 22:16:34 +1000

    Add LIFX.discover function
    Sends out a GetService broadcast and logs the response headers
    and payload as binary to standard out.

--- lib/lifx.ex
+++ lib/lifx.ex
@@ -1,2 +1,28 @@
 defmodule LIFX do
+  def discover do
+    {_, socket} = :gen_udp.open(56700, [:binary, active: true, broadcast: true])
+    bin = LIFX.LAN.Device.GetService.binary
+    :gen_udp.send(socket, {255,255,255,255}, 56700, bin)
+
+    check_mailbox
+    :gen_udp.close(socket)
+  end
+
+  def check_mailbox do
+    receive do
+      {:udp, _, _, 56700, bin} -> parse_binary(bin)
+    after
+      2_000 -> "done"
+    end
+  end
+
+  def parse_binary(bin) do
+    <<h::size(288), payload::binary>> = bin
+    header = <<h::size(288)>>
+
+    IO.inspect header
+    IO.inspect payload
+
+    check_mailbox
+  end
 end
